8f7a4c2bfb9fd370f4ad5f5169d113dfde41c6b0
Ayran Andrade <ayran.andrade@guiabolso.com.br>
2020-04-05 16:28:14 -0300

    Add grid from calculator

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,5 +1,66 @@
-import { Component } from "react";
+import * as React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
-export class Calculator extends Component {
+import { ButtonCalculator } from "./ButtonCalculator";
 
+export class Calculator extends React.Component {
+    render() {
+        return (
+            <Container fluid>
+                <Row>
+                    <Col>
+                        <ButtonCalculator label="7"/>
+                    </Col>
+                    <Col>
+                        <ButtonCalculator label="8"/>
+                    </Col>
+                    <Col>
+                        <ButtonCalculator label="9"/>
+                    </Col>
+                    <Col>
+                        <ButtonCalculator label="/"/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <ButtonCalculator label="4"/>
+                    </Col>
+                    <Col>
+                        <ButtonCalculator label="5"/>
+                    </Col>
+                    <Col>
+                        <ButtonCalculator label="6"/>
+                    </Col>
+                    <Col>
+                        <ButtonCalculator label="*"/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <ButtonCalculator label="1"/>
+                    </Col>
+                    <Col>
+                        <ButtonCalculator label="2"/>
+                    </Col>
+                    <Col>
+                        <ButtonCalculator label="3"/>
+                    </Col>
+                    <Col>
+                        <ButtonCalculator label="-"/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <ButtonCalculator label="0"/>
+                    </Col>
+                    <Col xs={6}>
+                        <ButtonCalculator label="="/>
+                    </Col>
+                    <Col>
+                        <ButtonCalculator label="+"/>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
 }
