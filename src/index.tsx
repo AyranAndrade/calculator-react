fff38aeed1ee57f1d35763058f08bf64ce81acdc
Ayran Andrade <ayran.andrade@guiabolso.com.br>
2020-04-05 15:23:30 -0300

    Add bootstrap to each page in the project

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import { Calculator } from "./components/Calculator";
 
 ReactDOM.render(
