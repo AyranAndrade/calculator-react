3f95569a07dd02bdab40a0434c34434321af07b9
Ayran Andrade <ayran.andrade@guiabolso.com.br>
2020-04-05 15:10:49 -0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    mode: "production",
+    entry: './src/index.tsx',
+    output: {
+        filename: './main.js',
+    },
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".jsx"]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "ts-loader"
+                    }
+                ]
+            },
+        ]
+    },
+};
