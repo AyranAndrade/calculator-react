47f36c6bad7f9c9f4d62529162cad03f705d6d81
Ayran Andrade <ayran.andrade@guiabolso.com.br>
2020-04-05 15:20:32 -0300

    Modify index to show new component calculator

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { Hello } from "./components/Hello";
+import { Calculator } from "./components/Calculator";
 
 ReactDOM.render(
-  <Hello name="Ayran Andrade" />,
+  <Calculator />,
   document.getElementById("root")
 );
